8e547d27b1cb337838b349e2dd218dfd131855f5
ValentinStoyanov <56118614+ValentinStoyanov@users.noreply.github.com>
2020-10-08 09:17:12 +0200

    Importing done

--- A1_12/src/main/java/A1_12/A1_12/Coordinate.java
+++ A1_12/src/main/java/A1_12/A1_12/Coordinate.java
@@ -4,62 +4,14 @@ import java.util.ArrayList;
 
 public class Coordinate {
 	
-
-	int x;
-	int y;
 	int value;
 	ArrayList<Boolean> neighbours;
 	
 	
-	public Coordinate(int x, int y, int value, ArrayList<Boolean> neighbours) {
-		super();
-		this.x = x;
-		this.y = y;
-		this.value = value;
-		this.neighbours = neighbours;
+	public Coordinate() {
+
 	}
 	
-	public Coordinate() {
-		
-	}
-
-
-	@Override
-	public String toString() {
-		return "Coordinate [x=" + x + ", y=" + y + ", value=" + value + ", neighbours=" + neighbours + "]";
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public int getValue() {
-		return value;
-	}
-
-	public void setValue(int value) {
-		this.value = value;
-	}
-
-	public ArrayList<Boolean> getNeighbours() {
-		return neighbours;
-	}
-
-	public void setNeighbours(ArrayList<Boolean> neighbours) {
-		this.neighbours = neighbours;
-	}
 	
 	
 
--- A1_12/src/main/java/A1_12/A1_12/Maze.java
+++ A1_12/src/main/java/A1_12/A1_12/Maze.java
@@ -1,6 +1,8 @@
 package A1_12.A1_12;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Maze {
 	
@@ -9,10 +11,18 @@ public class Maze {
 	int max_n;
 	ArrayList<int[]> mov;
 	ArrayList<String> id_mov;
-	Object cells;
+	HashMap<String, Coordinate> cells;
 	
 	public Maze() {
 
+	}
+
+	public HashMap<String, Coordinate> getCells() {
+		return cells;
+	}
+
+	public void setCells(HashMap<String, Coordinate> cells) {
+		this.cells = cells;
 	}
 
 	@Override
@@ -21,50 +31,7 @@ public class Maze {
 				+ ", cells=" + cells + "]";
 	}
 
-	public int getRows() {
-		return rows;
-	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-	public int getCols() {
-		return cols;
-	}
-
-	public void setCols(int cols) {
-		this.cols = cols;
-	}
-
-	public int getMax_n() {
-		return max_n;
-	}
-
-	public void setMax_n(int max_n) {
-		this.max_n = max_n;
-	}
-
-	public ArrayList<int[]> getMov() {
-		return mov;
-	}
-
-	public void setMov(ArrayList<int[]> mov) {
-		this.mov = mov;
-	}
-
-	public ArrayList<String> getId_mov() {
-		return id_mov;
-	}
-
-	public void setId_mov(ArrayList<String> id_mov) {
-		this.id_mov = id_mov;
-	}
-
-	public Object getCells() {
-		System.out.println("tamaño cells ");
-		return cells;
-	}
 
 
 
